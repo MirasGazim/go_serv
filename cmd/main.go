bc73c79aad87db38d8e31e0a29a4e88f0290311d
Газимов Диас <gvzumdias@gmail.com>
2025-05-06 20:50:32 +0500

    it is good

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/Gvzum/todo-app"
 	"github.com/Gvzum/todo-app/pkg/handler"
+	"github.com/Gvzum/todo-app/pkg/repository"
+	"github.com/Gvzum/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
